233ee3d122aab00bac0ce521f458104a6c7148f1
tyagi788898-coder <tyagi788898@gmail.com>
2026-08-04 11:50:19 +0530

    Update MainActivity.kt

--- app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
+++ app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
@@ -3,17 +3,22 @@ package com.institutional.tradingjournal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import com.institutional.tradingjournal.ui.navigation.NavGraph
-import com.institutional.tradingjournal.ui.theme.OrderflowTheme
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OrderflowTheme {
-                NavGraph()
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val navController = rememberNavController()
+                    NavGraph(navController = navController)
+                }
             }
         }
     }
