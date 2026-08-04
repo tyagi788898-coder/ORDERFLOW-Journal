b01386cc06f12b8df61385fd8198b42caeb93a54
tyagi788898-coder <tyagi788898@gmail.com>
2026-08-04 09:43:10 +0530

    Update NavGraph.kt

--- app/src/main/java/com/institutional/tradingjournal/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/institutional/tradingjournal/ui/navigation/NavGraph.kt
@@ -1,15 +1,9 @@
 package com.institutional.tradingjournal.ui.navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,8 +12,6 @@ import androidx.navigation.compose.rememberNavController
 import com.institutional.tradingjournal.ui.components.BottomNavItem
 import com.institutional.tradingjournal.ui.components.OrderflowBottomBar
 import com.institutional.tradingjournal.ui.screens.*
-import com.institutional.tradingjournal.ui.theme.DarkBackground
-import com.institutional.tradingjournal.ui.theme.GoldPrimary
 import com.institutional.tradingjournal.ui.viewmodel.StrategyViewModel
 import com.institutional.tradingjournal.ui.viewmodel.TradeViewModel
 
@@ -59,7 +51,7 @@ fun NavGraph() {
                 )
             }
             composable(BottomNavItem.Journal.route) {
-                StrategyManagerScreen(viewModel = strategyViewModel)
+                JournalChecklistScreen()
             }
             composable(BottomNavItem.Calendar.route) {
                 CalendarAnalyticsScreens(viewModel = tradeViewModel, isCalendarView = true)
