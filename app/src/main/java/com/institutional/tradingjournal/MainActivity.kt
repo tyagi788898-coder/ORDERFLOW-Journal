7712c07d7fecab89cf8a3be2194ccecaa8fedec4
tyagi788898-coder <tyagi788898@gmail.com>
2026-08-04 08:11:19 +0530

    Update MainActivity.kt

--- app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
+++ app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
@@ -3,17 +3,8 @@ package com.institutional.tradingjournal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.institutional.tradingjournal.ui.navigation.NavGraph
+import com.institutional.tradingjournal.ui.theme.OrderflowTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,33 +13,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OrderflowTheme {
-                MainScreen()
+                NavGraph()
             }
         }
     }
 }
-
-@Composable
-fun OrderflowTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        content = content
-    )
-}
-
-@Composable
-fun MainScreen() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF0D0D0D)), // Deep Black Institutional Theme
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = "ORDERFLOW V7 ULTIMATE",
-            color = Color(0xFFFFD700), // Gold Accent
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Bold
-        )
-    }
-}
-
