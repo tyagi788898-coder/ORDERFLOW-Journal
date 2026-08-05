94cb5ef5028290d4262459de2f9ae12f6117844a
tyagi788898-coder <tyagi788898@gmail.com>
2026-08-05 21:32:35 +0530

    Update MainActivity.kt

--- app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
+++ app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
@@ -3,22 +3,28 @@ package com.institutional.tradingjournal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.institutional.tradingjournal.ui.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Force window background programmatically to eliminate any white flash
+        window.decorView.setBackgroundColor(android.graphics.Color.parseColor("#090A0F"))
+        
         setContent {
-            MaterialTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    val navController = rememberNavController()
-                    NavGraph(navController = navController)
-                }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF090A0F)) // Immediate Dark Layer
+            ) {
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
     }
