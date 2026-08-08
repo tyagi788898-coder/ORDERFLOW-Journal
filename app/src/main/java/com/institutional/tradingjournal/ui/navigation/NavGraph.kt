3edc74e3e0e8279f9fde13459cbf9dbf9516eab9
tyagi788898-coder <tyagi788898@gmail.com>
2026-08-08 18:38:04 +0530

    Update NavGraph.kt

--- app/src/main/java/com/institutional/tradingjournal/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/institutional/tradingjournal/ui/navigation/NavGraph.kt
@@ -7,7 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.institutional.tradingjournal.model.TradeEntry
 import com.institutional.tradingjournal.ui.screens.CalendarAnalyticsScreens
-import com.institutional.tradingjournal.ui.screens.DashboardScreen
+import com.institutional.tradingjournal.ui.screens.DashboardScreens
 import com.institutional.tradingjournal.ui.screens.JournalChecklistScreen
 import com.institutional.tradingjournal.ui.screens.SettingsScreen
 import com.institutional.tradingjournal.ui.screens.SplashScreen
@@ -47,7 +47,7 @@ fun OrderflowNavGraph(
         }
 
         composable(Screen.Dashboard.route) {
-            DashboardScreen(
+            DashboardScreens(
                 isDark = isDark,
                 tradeList = tradeList
             )
@@ -77,4 +77,3 @@ fun OrderflowNavGraph(
         }
     }
 }
-
