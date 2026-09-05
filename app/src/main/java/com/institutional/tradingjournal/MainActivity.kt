64dcdb6f1991306a49373d0c7b478fe0ba4c047e
tyagi788898-coder <tyagi788898@gmail.com>
2026-09-05 09:10:05 +0530

    Update MainActivity.kt

--- app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
+++ app/src/main/java/com/institutional/tradingjournal/MainActivity.kt
@@ -16,7 +16,9 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.institutional.tradingjournal.ui.components.OrderflowBottomBar
 import com.institutional.tradingjournal.ui.navigation.OrderflowNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
